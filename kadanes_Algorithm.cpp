0c0fda61840fdb81f65dd0fe11ecc7729d5e60dc
Ritik Raj Soni <81621841+soniritik354@users.noreply.github.com>
2022-02-10 20:19:28 +0530

    Update kadanes_Algorithm.cpp

--- kadanes_Algorithm.cpp
+++ kadanes_Algorithm.cpp
@@ -1,3 +1,5 @@
+// Kadane's algorithm is the most efficient algorithm to find max sum of contiguous elements of an array
+//Brute force process would involve using a double for loop leading to an O(n^2) Time Complexity but here we have done it in O(n).
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -11,7 +13,9 @@ int main() {
 	}
 	int max_sum = INT_MIN;
 	int curr_sum = 0;
-	for(int i=0;i<6;i++){
+	//We maintain a running sum and after each iteration we check if it is greater than max_sum -> we substtute max_sum as curr_sum.
+	//Also if we get negative curr_sum we initialize the curr sum to zero as the negative part would contribute in reducing the max_sum. 
+	for(int i=0;i<n;i++){
 		curr_sum += arr[i];
 		if(curr_sum>max_sum)
 			max_sum = curr_sum;
