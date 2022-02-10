964d10d669de6966f0077fe0a3283f6ee410375e
Ritik Raj Soni <81621841+soniritik354@users.noreply.github.com>
2022-02-10 20:37:47 +0530

    Add files via upload

---
+++ insertion_sort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(vector<int> v){
+	for(int i=1;i<v.size();i++){
+		int temp = v[i];
+		int j = i-1;
+		while(j>=0 && v[j]>temp){
+			v[j+1] = v[j];
+			j--;
+		}
+		v[j+1] = temp;
+	}
+	for(int i=0;i<v.size();i++){
+		cout<<v[i]<<" ";
+	}
+}
+
+int main(){
+	vector<int> v = {8,4,1,5,9,2};
+	insertion_sort(v);
+	return 0;
+}
