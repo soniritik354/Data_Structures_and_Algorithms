0bb8ac79091a85ec2676b459b26e60e03027f6a0
Ritik Raj Soni <81621841+soniritik354@users.noreply.github.com>
2022-05-16 19:17:38 +0530

    Update Patterns_Questions.cpp

--- Patterns_Questions.cpp
+++ Patterns_Questions.cpp
@@ -28,3 +28,43 @@ int main() {
     return 0;
 }
 
+int main() {
+    //Half pyramid using alphabets
+    char alphabet = 'A';
+    char last = 'E';
+    
+    for(int i=1;i<=last-'A'+1;i++){
+        for(int j=1;j<=i;j++)
+            cout<<alphabet<<" ";
+        ++alphabet;
+        cout<<endl;
+    }
+}
+
+int main() {
+    //Inverted half pyramid using stars
+    int n=5;
+    while(n>=1){
+        for(int j=1;j<=n;j++){
+            cout<<'*'<<" ";
+        }
+        --n;
+        cout<<endl;
+        
+    }
+    
+
+    return 0;
+}
+
+int main() {
+    //Inverted half pyramid using numbers
+    int n=5;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
